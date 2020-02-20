f65a30344ab96ded2c94bd2753ca0e80fe6dfdec
Domen Golob <domen.golob@outfit7.com>
2020-02-20 19:08:08 +0100

    start

--- Drawing/Drawing/Program.cs
+++ Drawing/Drawing/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.IO;
 using System.Windows.Controls;
 
 namespace Drawing {
@@ -6,9 +8,22 @@ namespace Drawing {
     public class Program {
 
         private readonly Canvas _canvas;
+        private readonly string[] _input;
+
+        private int _numberOfBooks;
+        private int _numberOfLibraries;
+        private int _daysForScanning;
+
+        private int[] _books;
 
         public Program(Canvas canvas) {
             this._canvas = canvas;
+            _input = File.ReadAllLines("input.in");
+            ParseInput();
+        }
+
+        private void ParseInput() {
+            
         }
 
         public void Run() {
@@ -24,6 +39,22 @@ namespace Drawing {
 
         private void OneTick() {
 
+        }
+
+    }
+
+    public struct Library {
+
+        public int ID;
+        public int SignUp;
+        public int BooksPerDay;
+        public int[] Books;
+
+        public Library(int id, int signUp, int booksPerDay, int[] books) {
+            ID = id;
+            SignUp = signUp;
+            BooksPerDay = booksPerDay;
+            Books = books;
         }
 
     }
